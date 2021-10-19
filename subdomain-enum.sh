7a99efdf59ed452baafd861fa56356763946d0d9
helmutye0 <86020593+helmutye0@users.noreply.github.com>
2021-10-19 15:51:32 -0400

    Update subdomain-enum.sh

--- subdomain-enum.sh
+++ subdomain-enum.sh
@@ -2,7 +2,6 @@ if [ -z "$1" ]; then echo "Need to supply target domain file--./subdomain-enum.s
 for d in $(cat $1 ); do sublist3r -d $d -o $d.txt ; cat $d.txt >> enum.txt ; rm $d.txt;done 
 for i in $(cat enum.txt ); do host $i | grep 'alias\|address' | grep -v IPv6 | cut -d " " -f 1,4,6 | sed 's/alias\s//g' | tr ' ' '\|' >> enum2.txt;done
 cat enum2.txt | sort | uniq >> live-hosts.txt
-#cat enum2.txt | grep -f $1 | cut -d " " -f 2 | sort | uniq >> live-ip.txt
 rm enum.txt
 rm enum2.txt
 fi
